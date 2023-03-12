b727ce1af8dcb68529b50d359c9524d5e85683c7
elenagutarova <elenakarimova000@gmail.com>
2023-03-12 13:40:57 +0700

    Task10

---
+++ Program.cs
@@ -0,0 +1,7 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine ("Введите число a  ");
+int a=Convert.ToInt32(Console.ReadLine());
+int a1=a/10;
+int a2=a1%10;
+Console.WriteLine(a2);
+
