69994f18c2ab6bc7fc21ea2081f81803ffd11e0c
ElenaGutarova <126686800+ElenaGutarova@users.noreply.github.com>
2023-03-12 13:59:18 +0700

    Update Task10.cs

--- Task10.cs
+++ Task10.cs
@@ -1,4 +1,4 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿// Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
 Console.WriteLine ("Введите число a  ");
 int a=Convert.ToInt32(Console.ReadLine());
 int a1=a/10;
