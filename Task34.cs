bba80fc4cf0d37bcee869df1ac215fa61633e397
ElenaGutarova <126686800+ElenaGutarova@users.noreply.github.com>
2023-03-23 19:39:16 +0700

    Update Task34.cs

--- Task34.cs
+++ Task34.cs
@@ -1,53 +1,28 @@
-// Задача 34: Задайте массив заполненный случайными положительными трёхзначными числами. Напишите программу, которая покажет количество чётных чисел в массиве.
-
-//[345, 897, 568, 234] -> 2
-int Prompt(string message)
+int c = 0;
+int[] array = new int[12];
+int result = 0;
+while (c < 12)
 {
- Console.Write(message);
- string readInput = Console.ReadLine();
- int result=int.Parse(readInput);
- return result;
+    int x = new Random().Next(100, 1000);
+    array[c] = x;
+    Console.Write($" {array[c]}");
+    c = c + 1;
 }
-int [] Fillarray (int length, int min, int max)
+c = 0;
+while (c < 12)
 {
-    int [] array =new int [length];
-    Random random=new Random();
-    for (int i = 0; i < length; i++)
-     {
-       array [i]=new Random().Next(100,1000);
-     } 
-    return array;
-}
-void Printarray (int [] array)    
-{
-    Console.Write ("[");
-    for (int i = 0; i < array.Length-1; i++)
+    if (c % 2 == 0)
     {
-        Console.Write ($"{array[i]},");
+        result = result + c;
     }
-    Console.Write ($"{array[array.Length-1]}");
-    Console.Write ("]");
+    c++;
 }
+Console.WriteLine("");
+Console.Write($"Количество четных элементов = {result}");
 
-    
 
-int length = Prompt ("Длина массива");
-int Min = Prompt ("Начальное значение для диапазона случайного числа ");
-int Max = Prompt ("Конечное значение для диапазона случайного числа ");
-int [] array = Fillarray (length, Min, Max);
-Printarray (array);
-int EvenNumbers (int [] array)
-{
- int count =0;
- for (int i = 0; i < count; i++)
- {
-  if (array[i]/2!=0)
-    {
-     count++;
-    }
-         
- }
- return count;
- Console.WriteLine ($"Количество четных элементов " +count.ToString ());
-}
-EvenNumbers (array);
+
+
+
+
+
